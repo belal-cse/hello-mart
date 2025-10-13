6c93f88ebff85cd8e4ff4ad2ee454653f6fdbd18
belal-cse <ahmadmdbelal991@gmail.com>
2025-10-13 17:33:46 +0530

    Add files via upload

---
+++ array_reverse_print.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int arr[6] = {10, 20, 30, 40, 50, 60};
+    for(int i=5;i>0;i--){
+        cout<<arr[i]<<"  ";
+        
+
+    }
+    
+
+
+}
